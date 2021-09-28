c481423a9ee78e54f6ebdefbdb0a99b8ae5b47e7
alwijein <alwijein@gmail.com>
2021-09-28 06:56:18 -0700

    splash screen

--- lib/screens/splash_screens/splash_screens.dart
+++ lib/screens/splash_screens/splash_screens.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:laundy_apps/constants/constants.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            SvgPicture.asset('assets/icons/courier2.svg'),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset('assets/icons/splash-logo.svg'),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
